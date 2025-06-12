8deba7ba6773a33f0996bf0d0ba97488d4e9970f
Gn GN <43342066-nibagodswill@users.noreply.replit.com>
2025-06-12 04:00:21 +0000

    Project completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	server: {
+		allowedHosts: [
+			"42a5b501-1985-40e7-87c5-fe7a6442fa11-00-j49i91h5s9po.kirk.replit.dev"
+		],
 		proxy: {
 			"/api": {
 				target: "http://localhost:5000",
